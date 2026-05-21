1073050e181d4e58820bcbd22241c730dd379219
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2026-05-21 09:52:32 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://maclinicbackend.onrender.com',
+  baseURL: 'https://maclinicbackend.onrender.com/api',
 });
 
 api.interceptors.request.use(
